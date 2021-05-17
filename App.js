2803d508c7a03824f197f979b03fe62b8d697e2f
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-05-17 07:23:42 -0700

    add basic app.js to render

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    // <Router>
+    //   <div>
+    //   </div>
+    // </Router>
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
